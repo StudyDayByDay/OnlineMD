a73f32fc7b5e0989f84a6b09ef4a3693ffe82246
lk <2995201114@qq.com>
2024-11-20 21:41:46 +0800

    feat: 新增左右分屏功能

--- src/components/MDEditor/index.vue
+++ src/components/MDEditor/index.vue
@@ -1,13 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SplitPanel from '@/components/SplitPanel/index.vue';
+</script>
 <template>
-  <div class="md-editor">MDEditor</div>
+  <div class="md-editor">
+    <split-panel>
+      <template #left> 左边 </template>
+      <template #right> 右边 </template>
+    </split-panel>
+  </div>
 </template>
 
 <style lang="less" scoped>
 .md-editor {
   height: 100%;
   width: 100%;
-  padding: 15px;
   border: 2px solid #ccc;
   border-radius: 5px;
 }
