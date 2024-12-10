b939140f7e3d4271deb03d47584d3504b394f3ca
lk <2995201114@qq.com>
2024-12-10 11:00:02 +0800

    Updates

--- assets/freemarker2-SS3yZD64.js
+++ assets/freemarker2-SS3yZD64.js
@@ -1,4 +1,4 @@
-import{m as f}from"./monaco-BD8xcNfb.js";/*!-----------------------------------------------------------------------------
+import{m as f}from"./monaco-BT46KGL1.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.0(f6dc0eb8fce67e57f6036f4769d92c1666cdf546)
  * Released under the MIT license
